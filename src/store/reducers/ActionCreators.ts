ddee576c2492b5e754176cc6333e49b20735b0aa
keithchasen <keithchasen89@gmail.com>
2024-10-16 21:13:41 +0200

    fetching api data using async thunk and extra reducers

--- src/store/reducers/ActionCreators.ts
+++ src/store/reducers/ActionCreators.ts
@@ -2,19 +2,34 @@ import { AppDispatch } from '../store';
 import axios from 'axios';
 import { IUser } from '../../models/IUser';
 import { userSlice } from './UserSlice';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
-export const fetchUsers = () => async (dispatch: AppDispatch) => {
-	try {
-		dispatch(userSlice.actions.usersFetching());
-		const response = await axios.get<IUser[]>(
-			'https://jsonplaceholder.typicode.com/users'
-		);
-		dispatch(userSlice.actions.usersFetchingSuccess(response.data));
-	} catch (e: unknown) {
-		dispatch(
-			userSlice.actions.usersFetchingError(
-				e instanceof Error ? e.message : String(e)
-			)
-		);
+// export const fetchUsers = () => async (dispatch: AppDispatch) => {
+// 	try {
+// 		dispatch(userSlice.actions.usersFetching());
+// 		const response = await axios.get<IUser[]>(
+// 			'https://jsonplaceholder.typicode.com/users'
+// 		);
+// 		dispatch(userSlice.actions.usersFetchingSuccess(response.data));
+// 	} catch (e: unknown) {
+// 		dispatch(
+// 			userSlice.actions.usersFetchingError(
+// 				e instanceof Error ? e.message : String(e)
+// 			)
+// 		);
+// 	}
+// };
+
+export const fetchUsers = createAsyncThunk(
+	'user/fetchAll',
+	async (_, thunkApi) => {
+		try {
+			const response = await axios.get<IUser[]>(
+				'https://jsonplaceholder.typicode.com/users'
+			);
+			return response.data;
+		} catch (e) {
+			return thunkApi.rejectWithValue("Error: Couldn't load the users");
+		}
 	}
-};
+);
--- src/store/reducers/UserSlice.ts
+++ src/store/reducers/UserSlice.ts
@@ -1,5 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { IUser } from '../../models/IUser';
+import { fetchUsers } from './ActionCreators';
 
 interface UserState {
 	users: IUser[];
@@ -17,18 +18,39 @@ export const userSlice = createSlice({
 	name: 'user',
 	initialState,
 	reducers: {
-		usersFetching(state) {
-			state.isLoading = true;
-		},
-		usersFetchingSuccess(state, action: PayloadAction<IUser[]>) {
-			state.isLoading = false;
-			state.error = '';
-			state.users = action.payload;
-		},
-		usersFetchingError(state, action: PayloadAction<string>) {
-			state.isLoading = false;
-			state.error = action.payload;
-		},
+		// usersFetching(state) {
+		// 	state.isLoading = true;
+		// },
+		// usersFetchingSuccess(state, action: PayloadAction<IUser[]>) {
+		// 	state.isLoading = false;
+		// 	state.error = '';
+		// 	state.users = action.payload;
+		// },
+		// usersFetchingError(state, action: PayloadAction<string>) {
+		// 	state.isLoading = false;
+		// 	state.error = action.payload;
+		// },
+	},
+	extraReducers: builder => {
+		builder
+			.addCase(
+				fetchUsers.fulfilled.type,
+				(state, action: PayloadAction<IUser[]>) => {
+					state.isLoading = false;
+					state.error = '';
+					state.users = action.payload;
+				}
+			)
+			.addCase(fetchUsers.pending.type, state => {
+				state.isLoading = true;
+			})
+			.addCase(
+				fetchUsers.rejected.type,
+				(state, action: PayloadAction<string>) => {
+					state.isLoading = false;
+					state.error = action.payload;
+				}
+			);
 	},
 });
 
